4ef257455ebb4298afdf138ec587075be4c56ee7
vijendraenng <vijendra.nbjwork@gmail.com>
2025-07-08 00:55:52 +0530

    added first perfect navbar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo1.svg";
 
 export const assets = {
   logo,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { assets } from "../assets/assets.js";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between  px-10 items-center bg-red-400">
-      <div>
-        <img className="w-32" src={assets.logo} alt="" />
+    <div className="flex justify-between my-4  px-10 items-center border-solid border-b-2 border-black">
+      <div className="flex flex-col items-center">
+        <img className="w-10" src={assets.logo} alt="" />
+        <div className="font-semibold">Flex Living</div>
       </div>
-      <div className="flex gap-10">
+      <div className="flex font-semibold gap-10">
         <div>Home</div>
         <div>Landlord</div>
         <div>Blog</div>
